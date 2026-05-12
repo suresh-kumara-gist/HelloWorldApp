a07b37584c1a26f7c74ea5c8eea415d54c01df7f
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-05-12 16:21:36 +0530

    issue fix

--- app/src/main/java/com/helloworld/app/MainActivity.kt
+++ app/src/main/java/com/helloworld/app/MainActivity.kt
@@ -1,27 +1,28 @@
 package com.helloworld.app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private var clickCount = 0
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupNavigation()
-    }
-    
-    private fun setupNavigation() {
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-    }
-    
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+        
+        textView.text = "Welcome to Hello World App!\nOptimized for Redmi Note 7"
+        
+        button.setOnClickListener {
+            clickCount++
+            textView.text = "Hello World! (Clicked $clickCount times)"
+        }
     }
 }
