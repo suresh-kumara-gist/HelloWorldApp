a45a1a3c63eaecf573a14044a8de49f71b992610
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-05-12 16:38:21 +0530

    issue fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,17 +13,12 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     
